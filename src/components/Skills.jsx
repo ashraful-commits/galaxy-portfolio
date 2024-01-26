86ce24e498e6fa9b997b90fc9a7ca8a8cce0d724
ashraful-commits <ashrfulalam10000@gmail.com>
2024-01-26 18:56:33 +0600

    updated skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,7 @@ const Skills = () => {
     <div className="absolute top-0 left-0 z-10 w-full h-full">
         <img className="object-cover w-full h-full" src="/galaxy.gif"/>
     </div>
-     <h2 className="text-white font-syncopate text-[32px] lg:text-[50px] md:text-[50px] leading-9 relative text-center z-30 reflection ">Professional skills</h2>
+     <h2 className="text-white font-syncopate group-hover:text-red-500 text-[32px] lg:text-[50px] md:text-[50px] leading-9 relative text-center z-30 reflection ">Professional skills</h2>
     <div className="relative z-20 grid items-center justify-between h-full grid-cols-2 mt-20 gap-x-1 gap-y-1 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5">
         <div className="relative z-0 flex items-center justify-center transition-all duration-500 ease-in-out bg-blue-600 border border-gray-900  hover:bg-blue-700 hover:bg-opacity-70 min-w-[40vw] md:min-w-[20vw] lg:min-w-[15vw] min-h-[25vh] bg-opacity-5"> 
             
